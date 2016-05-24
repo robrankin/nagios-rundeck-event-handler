29f291a95c4acd9d562541356354b9d84e9ad856
Rob Rankin <github@undertow.ca>
2016-05-24 11:38:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Nagios Event Handler for RunDeck
+===
+
+This is a simple and ugly event handler to fire off a single RunDeck job.  This satisfies my needs, but most probably won't satisfy yours.  Contributions and PRs welcome.
+
+Nagios Setup
+---
+
+Nagios Command Definition
+
+```
+define command {
+    command_name rd_run_job
+    command_line $USER1$/rd_run_job.pl "$SERVICESTATE$" "$SERVICESTATETYPE$" "$SERVICEATTEMPT$" "<rundeck username>" "<rundeck password>" "<rundeck url>" "$_SERVICEJOBID$" "$HOSTNAME$" "DEBUG"
+}
+```
+
+* `$SERVICESTATE$` Nagios Macro providing service state
+* `$SERVICESTATETYPE$` Nagios Macro providing state type (CRITICAL SOFT versus CRITICAL HARD for example)
+* `$SERVICEATTEMPT$` Nagios Macro for number of check attempts (SOFT 2 or 3 checks for example)
+* `<rundeck username>` Your RunDeck user name that can run the jobs
+* `<rundeck password>` Your RunDeck password
+* `<rundeck url>` Base RunDeck URL (https://rundeck.example.org:4440)
+* `$_SERVICEJOBID$` The RunDeck Job ID passed by the Nagios Service Check
+* `$HOSTNAME$` Nagios Macro HOSTNAME, acts as a RunDeck Filter (i.e.: `name:$HOSTNAME$`)
+* `DEBUG` RunDeck job DEBUG level.
+
+
+Nagios Service Definition
+
+```
+define service {
+    host_name           host1.example.com
+    service_description Apache Service
+    check_command       check_http_port!80
+    event_handler       rd_run_job
+    _JOBID              e1a8c9ec-379d-46ca-9e33-6096bde0a17a
+}
+```
+
+Add the `event_handler` and `_JOBID` Nagios parameters to your Service Check(s).
+
+
+Event Handler
+---
+The Event Handler will execute on either:
+
+* CRITICAL SOFT with at least 3 check attempts
+* CRITICAL HARD
+
+Command Line Testing
+---
+```
+./rd_run_job.pl "CRITICAL" "HARD" "3" "nagios" "password" "http://rundeck.example.com:4440" "e1a8c9ec-379d-46ca-9e33-6096bde0a17a" "name:hostname.example.com" "DEBUG"
+```
+
+
+
+ToDo
+---
+1. Abstract RunDeck Username/Password
+2. Abstract RunDeck URL
+3. Make it work with Host Checks, not just Service Checks
---
+++ rd_run_job.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use LWP::UserAgent;
+use HTTP::Cookies;
+use JSON::XS;
+
+# nagios exit codes
+use constant EXIT_OK       => 0;
+use constant EXIT_WARNING  => 1;
+use constant EXIT_CRITICAL => 2;
+use constant EXIT_UNKNOWN  => 3;
+
+my $debug      = undef;
+my $state      = $ARGV[0];
+my $statetype  = $ARGV[1];
+my $attemptnum = $ARGV[2];
+my $username   = $ARGV[3];
+my $password   = $ARGV[4];
+my $rundeck    = $ARGV[5];
+my $jobid      = $ARGV[6];
+my $filter     = $ARGV[7];
+my $logLevel   = $ARGV[8];
+my $argString  = $ARGV[9];
+my $asUser     = $ARGV[10];
+
+my $rd_auth_url      = $rundeck . '/j_security_check';
+my %rd_auth_headers  = ( 'Content-Type' => 'application/x-ww-form-urlencoded' );
+my %rd_auth_body     = ( 'j_username' => $username, 'j_password' => $password );
+my $rd_run_url       = $rundeck . "/api/1/job/$jobid/run";
+my %rd_run_headers   = ( 'Content-Type' => 'application/json' );
+
+my $coder     = JSON::XS->new->ascii->pretty->allow_nonref;
+my $useragent = LWP::UserAgent->new(
+  cookie_jar    => HTTP::Cookies->new,
+);
+
+###
+### Main
+###
+
+if ($state eq "CRITICAL") {
+  if ($debug) { print "State: CRITICAL\n"; }
+  if ( ($statetype eq "SOFT" && $attemptnum >= 3) || ($statetype eq "HARD") ) {
+    if ($debug) { print "Type: $statetype Num: $attemptnum\n"; }
+    my $rd_auth_response = web_request ('auth', 'POST', $rd_auth_url, \%rd_auth_body, \%rd_auth_headers );
+    # Whackjob RunDeck API returns HTTP 200 on successfully FAILING auth. http://rundeck.org/2.6.6/api/index.html#password-authentication
+    if ( $rd_auth_response->request()->uri() =~ m#/user/error# || $rd_auth_response->request()->uri() =~ m#/user/login# ) {
+      print "RunDeck Auth Failed\n"; exit EXIT_CRITICAL;
+    }
+    my $rd_run_body = build_rundeck_body ( $argString, $logLevel, $asUser, $filter );
+    my $rd_run_response = web_request ('run', 'POST', $rd_run_url, $rd_run_body, \%rd_run_headers );
+    if ( ! $rd_run_response->is_success ) { print "RunDeck Run Request Failed\n"; exit EXIT_CRITICAL; }
+  } else {
+  exit EXIT_OK;
+  }
+}
+
+
+###
+### Subs
+### Ugliness hidden below here
+
+
+sub build_rundeck_body {
+  my ($argString, $logLevel, $asUser, $filter) = @_;
+
+  my $rundeck_body->{'argString'} = $argString;
+     $rundeck_body->{'logLevel'}  = $logLevel;
+     $rundeck_body->{'asUser'}    = $asUser;
+     $rundeck_body->{'filter'}    = $filter;
+
+  my $json_body = encode_json $rundeck_body;
+
+  return $json_body;
+}
+
+
+sub web_request {
+  my ($type, $method, $url, $body, $headers) = @_;
+  my $response = '';
+
+  if ($type eq "auth") {
+    $useragent->requests_redirectable ( ['GET', 'HEAD', 'POST']  );
+    $useragent->default_header ( 'Content-Type' => $headers->{'Content-Type'} );
+    $response = $useragent->post( $url, $body );
+  } elsif ($type eq "run") {
+    my $request = HTTP::Request->new($method => $url);
+    $request->content_type( $headers->{'Content-Type'} );
+    $request->content($body);
+    $response = $useragent->request($request);
+  }
+
+  return $response;
+}
